a6a02bf44152724a2947ab80f2620bfeb56fe765
ArthurSenai <arthur_f_rodrigues@estudante.sesisenai.org.br>
2022-05-06 17:55:06 -0300

    4°

--- src/pages/Login/LoginScreen.js
+++ src/pages/Login/LoginScreen.js
@@ -2,16 +2,63 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
 
-class LoginScreen  extends Component {
+
+let salvar = () => {
+    console.log(document);
+
+}
+
+class LoginScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isGoing: true,
+            numberOfGuests: 7
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+        
+    }
+    
+
     render() {
         return (
-            <div className="title">
-                <h4>Login</h4>
-                <p>Aqui é o Login sabe ? Bota o nomezin e senha ai pra entrar :)</p>
-                <Link to="/HomeScreen">Dale pro Home</Link>
-            </div>
+            <form>
+                <label>
+                    Estão indo:
+                <input
+                        name="isGoing"
+                        type="checkbox"
+                        // checked={this.state.isGoing}
+                        onChange={this.handleInputChange} />
+                </label>
+                <br />
+                <label>
+                    Número de convidados:
+                <input
+                        name="numberOfGuests"
+                        type="number"
+                        // value={this.state.numberOfGuests}
+                        onChange={this.handleInputChange} />
+                </label>
+
+                <input type="submit" value="Enviar" onClick={console.log(this.handleInputChange.value)}
+                />
+            </form>
         );
     }
+
 }
 
 export default LoginScreen;
