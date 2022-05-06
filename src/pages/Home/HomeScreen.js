b46e56a35c2efa686cadb63a7dd856e23b2905f6
ArthurSenai <arthur_f_rodrigues@estudante.sesisenai.org.br>
2022-05-05 22:23:30 -0300

    2° commit

--- src/pages/Home/HomeScreen.js
+++ src/pages/Home/HomeScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-import './HomeStyles.css';
+
+import './HomeStyle.css';
 import raio_azul from "../../assets/raio_azul.png";
+
 
 const HomeScreen = () => (
     <container>
@@ -10,16 +12,17 @@ const HomeScreen = () => (
             Título
         </header>
         <div className='barra_lateral'>
-            <header className='header_barra'>
+            <div className='header_barra'>
                 <img src={raio_azul} className='logo' />
-            </header>
+            </div>
         </div>
         <div className='corpo'>
             <h4>Home</h4>
             <p>Aqui é o Home sabe ?</p>
-            <Link to="/LoginScreen">Bora pro login ??</Link> <br></br>
-            <Link to="/">Quem sabe, voltar pro App ? ?</Link>
-            <Link to="/Teste1">Testar é importante também né ?</Link>
+            <Link to="/LoginScreen">Bora pro login ??</Link>                    <br></br>
+            <Link to="/">Quem sabe, voltar pro App ? ?</Link>                   <br></br>
+            <Link to="/Teste1">Testar é importante também né ?</Link>           <br></br>
+            <Link to="/RegisterScreen">Bora registraarr</Link>           <br></br>
         </div>
     </container>
 );
