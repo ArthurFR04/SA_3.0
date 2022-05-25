023c9fb05e852fded38f91f01f2728087e2f5d26
ArthurSenai <arthur_f_rodrigues@estudante.sesisenai.org.br>
2022-05-24 23:18:01 -0300

    começando login

--- src/pages/Login/LoginScreen.js
+++ src/pages/Login/LoginScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 // import { Link } from "react-router-dom";
 
+import './LoginStyle.css'
+
 let findAll = () => {
 
     fetch('http://localhost:3000/api/usuario',)
@@ -14,13 +16,50 @@ let findAll = () => {
 
 // let RequesitarNoBackzin = () => {console.log(findAll)}
 
+
+
+let alerta = () => {
+    alert('Não sei fazer isso ainda ksks')
+}
+
 const LoginScreen = () => (
-    <div>
+    <div className="container">
+        <div className="col-central">
 
-        {/* <button onClick={RequesitarNoBackzin}>
-            Enviar
-        </button> */}
+            <h1 className="login">
+                LOGIN
+            </h1>
 
+            <input
+                className="email-user"
+                type="text"
+                placeholder="  email ou usuário"
+            />
+            <br></br>
+
+            <input
+                className="senha"
+                type="password"
+                placeholder="  senha"
+            />
+            <br></br>
+
+            <div className="btn">
+                <button
+                    className="btn_entrar"
+                // onClick=""
+                >
+                    ENTRAR
+                </button>
+            </div>
+
+            <p>OU</p>
+
+            <p onClick={alerta}>
+                entrar com Google
+            </p>
+
+        </div>
     </div>
 );
 
