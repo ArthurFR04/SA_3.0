a89359ff7880a29b6cdb147a49861f07c8aa6ba7
Ilariew13 <lariwazlawick@gmail.com>
2022-08-17 17:52:04 -0300

    register

--- src/pages/Register/RegisterScreen.js
+++ src/pages/Register/RegisterScreen.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import './RegisterStyle.css';
+import React, { useState } from 'react';
+import styles from './RegisterStyle.module.css';
+
+
+import { RiLock2Fill } from 'react-icons/ri';
+import { FaUserAlt } from 'react-icons/fa';
 
 let validado = false
 let inputs
 
 let validar = () => {
 
-console.log('começando validação');
+    console.log('começando validação');
     for (let i = 0; i < inputs.length; i++) {
-console.log(inputs[i]);
+        console.log(inputs[i]);
         if (inputs[i].value !== false) {
             if (inputs[3].value === inputs[4].value) {
                 validado = true
@@ -45,9 +49,9 @@ let cadastrar = async () => {
                 'Content-type': 'application/json; charset=UTF-8',
             },
         })
-        .catch((error) => {
-            console.log(error)
-        })
+            .catch((error) => {
+                console.log(error)
+            })
             .then((response) => response.json())
             .then((json) => console.log(json))
     }
@@ -55,38 +59,120 @@ let cadastrar = async () => {
     validado = false
 }
 
+// const [values, setValues] = 0 // 0 USADO PARA COMENTAR O USE STATE
+// useState('')
+
+const changeInput = (value) => {
+    // setValues((prevValue) => ({
+    //     ...prevValue,
+    //     [value.target.name]: value.target.value,
+    // }))
+}
+
+// let values2;                                                            let Login_values = 0; let entrar;
+const salvar = () => {
+    // values2 = values
+    // Login_values.value = values
+    // document.getElementById("loading").style.display = "flex"
+    // entrar()
+}
+
 const RegisterScreen = () => (
 
-    <div className="centralizar">
-        <form>
-            <input
-                type="text"
-                placeholder='Nome'
-            />
+    <div className={styles.body}>
 
-            <input
-                type="text"
-                placeholder='Sobrenome'
-            />
+        <div className={styles.banner}>
+            <h1>ElTEC</h1>
+        </div>
 
-            <input
-                type="text"
-                placeholder='Email'
-            />
+        <div className={styles.container}>
 
-            <input
-                type="password"
-                placeholder='Senha'
-            />
+            {/* <div className={styles.info}> */}
 
-            <input
-                type="password"
-                placeholder='Repita a senha'
-            />
-        </form>
-            <button onClick={cadastrar}>
-                Salvar
+            <h1 className={styles.cadastro}>
+                CADASTRO
+            </h1>
+
+            <div className={styles.div_inputs}>
+
+                <span className={styles.spanEmail}>
+
+                    <label className={styles.labelUser} >
+                        <label className={styles.circuloUser}>
+                            <FaUserAlt />
+                        </label>
+                    </label>
+
+                    <input
+                        type="text"
+                        name="email"
+                        onChange={changeInput}
+                        className={styles.email}
+                        placeholder="email"
+                    />
+                </span>
+
+                <span className={styles.spanSenha}>
+
+                    <input
+                        type="password"
+                        name="senha"
+                        onChange={changeInput}
+                        className={styles.senha}
+                        placeholder="senha"
+
+                    />
+                    <label className={styles.labelCadeado} >
+                        <label className={styles.circuloCadeado}>
+                            <RiLock2Fill />
+                        </label>
+                    </label>
+                </span>
+
+                <span className={styles.spanConfSenha}>
+                    <input
+                        type="password"
+                        name="confirmarSenha"
+                        onChange={changeInput}
+                        className={styles.senha}
+                        id={styles.confirm}
+                        placeholder=" confirmar senha"
+                    />
+
+                    <label className={styles.labelCadeado} >
+                        <label className={styles.circuloCadeado}>
+                            <RiLock2Fill />
+                        </label>
+                    </label>
+                </span>
+
+            </div>
+
+            <br></br>
+
+            {/* <div className={styles.btn}> */}
+            <button
+                className={styles.btn_entrar}
+                onClick={salvar}
+            >
+                ENTRAR
             </button>
+            {/* </div> */}
+
+            <p>OU</p>
+
+            <p
+            // onClick={alerta}
+            >
+                entrar com Google
+            </p>
+
+            {/* </div> */}
+        </div>
+
+        {/* <div id='loading' className={styles2.loading}>
+        <AiFillThunderbolt className={styles2.loading_icon} id='foguetin'/>
+    </div> */}
 
     </div>
 );
